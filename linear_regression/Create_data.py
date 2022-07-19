791cc7aae79a69391bf12107920bf504f78572b5
周晓磊01 <709164018@qq.com>
2022-07-19 21:10:45 +0800

    2022.07.19

--- linear_regression/Create_data.py
+++ linear_regression/Create_data.py
@@ -1,22 +1,26 @@
 import torch
-import numpy
+import numpy as np
+from torch import nn
 from d2l import torch as d2l
 import random
 
 """
-pretest
+pretest, wrong
 """
+
+
+def add_to_class(Class):
+    def wrapper(obj):
+        setattr(Class, obj.__name__, obj)
+    return wrapper
+
+
 class HyperParameters:
-    def save_hyperparameters(self, ignore = []):
+
+    def save_hyperparameters(self, ignore=[]):
         raise NotImplemented
 
-
-"""
-Create a SyntheticRegressionDate
-"""
-
-
-class DataModule(d2l.HyperParameters):
+class DataModule():
     def __init__(self, root = '../data', num_workers = 4):
         self.save_hyperparameters()
 
@@ -30,15 +34,95 @@ class DataModule(d2l.HyperParameters):
         return self.get_dataloader(train = False)
 
 
-class SyntheticRegressionData(d2l.DataModle):
-    def __init__(self, w, b, noise = 0.01, num_train = 1000, num_val = 1000, batch_size = 32):
-        super().__init__()
-        self.save_hyperparameters()
-        n = num_train + num_val
-        self.X = torch.randn(n, len(w))
-        noise = torch.randn(n, 1) * noise
-        self.y = torch.matmul(self.X, w.reshape((-1, 1))) + b + noise
+"""
+Create a SyntheticRegressionDate
+"""
+
+
+def synthetic_data(w, b, num_examples):
+    X = torch.normal(0, 1, (num_examples, len(w)))
+    y = torch.matmul(X, w) +b
+    y += torch.normal(0, 0.01, y.shape)
+    return X, y.reshape((-1,1))
+
+
+"""
+load the dataset
+"""
+
+
+def data_iter(batch_size, features, labels):
+    num_examples = len(features)
+    indices = list(range(num_examples)) # [0, 1, 2......]
+    random.shuffle(indices)
+    for i in range(0, num_examples, batch_size):
+        batch_indices = torch.tensor(indices[i: min(i + batch_size, num_examples)])
+        yield features[batch_indices], labels[batch_indices]
+
+
+
+'''
+initialize the parameters from model 
+'''
+w = torch.normal(0, 0.01, size = (2, 1), requires_grad=True)
+b = torch.zeros(1, requires_grad=True)
+
+
+'''
+Define model
+'''
+def linreg(X, W, b):
+    ''' linear regression'''
+    return torch.matmul(X, w) +b
+
+
+'''
+Loss function
+'''
+
+def squared_loss(y_hat, y):
+    '''averaged loss'''
+    return (y_hat - y.reshape(y_hat.shape))** 2 / 2
+
+'''
+optimizer
+'''
+def sgd(params, lr, batch_size):
+    with torch.no_grad(): # needn't to compute and store the gradient for saving the resource
+        for param in params:
+            param -= lr * param.grad / batch_size
+            param.grad.zero_() #graduent was turned to be zero
+
+
 
 if __name__ == '__main__':
-    data = SyntheticRegressionData(w = torch.tensor([2, -3.4]), b = 4.2)
-    print(("features:", data.X[0], '\nlabel:', data.y[0]))
+    true_w = torch.tensor([2,-3.4])
+    true_b = 4.2
+    features, labels = synthetic_data(true_w, true_b, 1000)
+
+    batch_size = 10
+    for X, y in data_iter(batch_size, features, labels):
+        print(X,"\n", y)
+        break
+
+    """
+    training
+    """
+
+    lr = 0.03
+    num_epochs = 3
+    net = linreg
+    loss = squared_loss
+
+    for epoch in range(num_epochs):
+        for X, y in data_iter(batch_size, features, labels):
+            l = loss(net(X, w, b), y)
+            l.sum().backward() # l.shape = (batch_size, 1)
+            sgd([w,b], lr, batch_size)
+        with torch.no_grad():
+            train_1 = loss(net(features, w, b), labels)
+            print(f'epoch{epoch + 1}, loss{float(train_1.mean()):f}')
+
+print(f'the loss about estimate w:{true_w - w.reshape(true_w.shape)}')
+print(f'the loss about estimate b:{true_b - b}')
+
